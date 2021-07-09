3aa373ca43014a54c43dc83168f6029ea0569302
y-solb <ysolb99@gmail.com>
2021-07-09 21:03:24 +0900

    refactor : change to axios

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 import App from './app.jsx';
 import '@fortawesome/fontawesome-free/js/all.js';
 import Youtube from './service/youtube';
+import axios from 'axios';
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: 'https://youtube.googleapis.com/youtube/v3',
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+
+const youtube = new Youtube(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,25 +1,30 @@
 class Youtube {
-  constructor(key) {
-    this.key = key;
-    this.getRequestOptions = { method: 'GET', redirect: 'follow' };
+  constructor(httpClient) {
+    this.youtube = httpClient;
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result = await response.json();
-    return result.items;
+    const response = await this.youtube.get('videos', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: 25,
+      },
+    });
+    return response.data.items;
   }
 
   async search(txt) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${txt}&type=video&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result = await response.json();
-    return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 25,
+        q: txt,
+        type: 'video',
+      },
+    });
+
+    return response.data.items.map((item) => ({ ...item, id: item.id.videoId }));
   }
 }
 
